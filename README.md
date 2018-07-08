614700646410f9d3c27f9d69daeef07a2190656c
erichht <erichht71@gmail.com>
2018-07-09 11:31:53 +1200

    Updated on 09/07/18: Fixed displaying problem of index page

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This Project repo followes below tutorial.
 * Original tutorial: https://yoember.com/
 
 # Library-app
+Library App is a FrontEnd project which bases on Ember.js framework, along with Bootstrap, Sass, Firebase and so on. Moreover, it uses Promise which is one of the ES6 features.
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-    responseMessage: '',
+    // responseMessage: '',
 
     model() {
         return this.store.createRecord('invitation');
@@ -13,7 +13,7 @@ export default Route.extend({
 
             newInvitation.save().then(() => this.transitionTo('index'));
 
-            this.controller.set('responseMessage',`Thank you! We've just saved your email address with the following id: ${this.controller.get('model.id')}`);
+            // this.controller.set('responseMessage',`Thank you! We've just saved your email address with the following id: ${this.controller.get('model.id')}`);
 
         },
 
